ef32ecda543d32c7934e42fa5fa9109b67d95235
ZAHRA MOHAMED KAMOORDEEN <zahra96mohd@gmail.com>
2024-10-17 18:16:35 +0400

    created student info page

---
+++ Pages/StudentInfo.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace StudentManager.Pages
+{
+    public class StudentInfoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
